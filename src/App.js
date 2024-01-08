6f99ad24a3472f0ab280114c2e8e8328c38d7d6f
Minh <4501104141@student.hcmue.edu.vn>
2024-01-08 15:48:10 +0700

    useState_gift

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
 
+const gifts = [
+  'CPU i9',
+  'RAM 32GB RGB',
+  'RGB Keyboard'
+]
 function App() {
+  const [gift, setGift] = useState()
+  const randomGift = () => {
+    const index = Math.floor(Math.random() * gifts.length)
+    setGift(gifts[index]);
+  }
   return (
     <div style={{ padding: 32 }}>
-
+      <h1>{gift || 'Chua co phan thuong'}</h1>
+      <button onClick={randomGift}>Lay phan thuong</button>
     </div>
   );
 }
