46baa94d60da82fefd7d45efe71d662d8f1bd220
Minh <4501104141@student.hcmue.edu.vn>
2024-01-09 21:29:36 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { useEffect, useRef, useState } from "react"
 
 function App() {
+  const [count, setCount] = useState(60)
+  const timerId = useRef()
+  const prevCount = useRef()
+  const h1Ref = useRef()
+  useEffect(() => {
+    prevCount.current = count
+  }, [count])
+  useEffect(() => {
+    console.log(h1Ref.current);
+  })
+  const handleStart = () => {
+    timerId.current = setInterval(() => {
+      setCount(prevCount => prevCount - 1)
+    }, 1000)
+    console.log('Start -> ', timerId.current);
+  }
+  const handleStop = () => {
+    clearInterval(timerId.current)
+    console.log('Stop -> ', timerId.current);
+  }
+  console.log(count, prevCount.current);
   return (
     <div style={{ padding: 32 }}>
-
+      <h1 ref={h1Ref}>{count}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
     </div>
   );
 }
