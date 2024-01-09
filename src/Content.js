8f947d2e4cc0cb78c093d323918e9c5f26bb3b04
Minh <4501104141@student.hcmue.edu.vn>
2024-01-09 16:21:50 +0700

    done

--- src/Content.js
+++ src/Content.js
@@ -9,61 +9,21 @@ import { useEffect, useState } from "react"
 //------------------
 // 1. Callback luon duoc goi sau khi component mounted
 // 2. Cleanup function luon duoc goi truoc khi component unmounted
-const tabs = ['posts', 'comments', 'albums', 'photos', 'todos', 'users']
 function Content() {
-    const [posts, setPosts] = useState([])
-    const [type, setType] = useState('posts')
-    const [showGoToTop, setShowGoToTop] = useState(false)
+    const [width, setWidth] = useState(window.innerWidth)
     useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/${type}`)
-            .then(res => res.json())
-            .then(posts => {
-                setPosts(posts)
-            })
-    }, [type])
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY >= 200) {
-                setShowGoToTop(true)
-            } else {
-                setShowGoToTop(false)
-            }
+        const handleResize = () => {
+            setWidth(window.innerWidth)
         }
-        window.addEventListener('scroll', handleScroll)
-        console.log('addEventListener');
+        window.addEventListener('resize', handleResize)
         //Cleanup function
         return () => {
-            window.removeEventListener('scroll', handleScroll)
-            console.log('removeEventListener');
+            window.removeEventListener('resize', handleResize)
         }
     }, [])
     return (
         <div>
-            {tabs.map(tab => (
-                <button
-                    key={tab}
-                    style={type === tab ? {
-                        color: '#fff',
-                        backgroundColor: '#333'
-                    } : {}}
-                    onClick={() => setType(tab)}
-                >
-                    {tab}
-                </button>
-            ))}
-            {posts.map(post => (
-                <li key={post.id}>{post.title || post.name}</li>
-            ))}
-            {showGoToTop && (
-                <button
-                    style={{
-                        position: 'fixed',
-                        right: 20,
-                        bottom: 20
-                    }}>
-                    Go to top
-                </button>
-            )}
+            <h1>{width}</h1>
         </div>
     )
 }
