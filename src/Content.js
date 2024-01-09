4892b424a3be6b5dffcabb4289a537b17eab1587
Minh <4501104141@student.hcmue.edu.vn>
2024-01-09 16:36:08 +0700

    done

---
+++ src/Content.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react"
+
+function Content() {
+    const [countdown, setCountdown] = useState(180)
+    useEffect(() => {
+        const timerId = setInterval(() => {
+            setCountdown(prevState => prevState - 1)
+            console.log('Countdown...');
+        }, 1000)
+        return () => clearInterval(timerId)
+    }, [])
+    return (
+        <div>
+            <h1>{countdown}</h1>
+        </div>
+    )
+}
+export default Content
