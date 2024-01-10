dd6fa32cd6d34066978adc543fd6064069146e36
Minh <4501104141@student.hcmue.edu.vn>
2024-01-10 13:31:42 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import { useState, memo, useCallback } from "react";
+import Content from "./Content";
 function App() {
+  const [count, setCount] = useState(0)
+  const handleIncrease = useCallback(() => {
+    setCount(prevCount => prevCount + 1)
+  }, [])
   return (
-    <div style={{ padding: 32 }}>
-
+    <div style={{ padding: '10px 32px' }}>
+      <Content onIncrease={handleIncrease} />
+      <h1>{count}</h1>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/Content.js
@@ -0,0 +1,11 @@
+import { memo } from "react";
+function Content({ onIncrease }) {
+    console.log('re-render');
+    return (
+        <>
+            <h2>Hello hihi</h2>
+            <button onClick={onIncrease}>Click me!</button>
+        </>
+    )
+}
+export default memo(Content)
