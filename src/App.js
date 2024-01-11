081b1602eb9ca67841e967295327c8916e8a6159
Minh <4501104141@student.hcmue.edu.vn>
2024-01-11 11:51:32 +0700

    done_1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-
+import { createContext, useState } from "react";
+import Content from "./Content";
+import './App.css'
+export const ThemeContext = createContext()
+//1. Create context
+//2. Provider
+//3. Consumer
 function App() {
+  const [theme, setTheme] = useState('dark')
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
   return (
-    <div style={{ padding: 32 }}>
-
-    </div>
+    <ThemeContext.Provider value={theme}>
+      <div style={{ padding: 32 }}>
+        <button onClick={toggleTheme}>Toggle theme</button>
+        <Content />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
